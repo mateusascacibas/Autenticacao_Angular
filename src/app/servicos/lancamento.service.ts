f988bec60c4267d478e0c4ae3989a7b0a63b8415
Mateus Ascacibas <mateus.asilva2001@hotmail.com>
2021-08-18 14:08:04 -0300

    Adicionado ação para exportar CSV.

--- src/app/servicos/lancamento.service.ts
+++ src/app/servicos/lancamento.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment.prod';
 import { HttpService } from './http.service';
+import { saveAs } from 'file-saver';
+import { Lancamentos } from '../componentes/models/lancamentos';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,5 +16,21 @@ export class LancamentoService {
   listarTodos(): Observable<any> {
     const id = this.httpService.obterIdUsuario(); 
     return this.http.get(env.apiBaseUrl + "api/lancamentos/funcionario/" + id +"?sort=id&dir=DESC", this.httpService.headers());
+  }
+
+ 
+  downloadCSV(lancamentos: Lancamentos[]){
+    const colunas='ID,Data,Hora,Tipo,Localizacao\n';
+    const linhas: string[] = [];
+    lancamentos.forEach(lanc =>{
+      const dataHora = lanc.data.split(" ");
+      const linha = `${lanc.id},${dataHora[0]},${dataHora[1]},${lanc.tipo},"${lanc.localizacao}"`; 
+      linhas.push(linha);
+    });
+
+    console.log(colunas + linhas.join('\n'));
+    const dados = colunas + linhas.join('\n');
+    const blob = new Blob([dados], { type: 'text/csv' });
+    saveAs(blob, 'lancamentos.csv');
   }
 }
